17a64fd894cebd5b5b85b2fd79a5c05dc41421d0
Kigi Chang <kigi.chang@gmail.com>
2013-04-19 18:01:57 +0800

    add relation annotation

---
+++ kigi-data/src/main/java/tw/kigi/data/annotation/BelongsTo.java
@@ -0,0 +1,18 @@
+package tw.kigi.data.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface BelongsTo {
+	String schema();
+	String foreignKey();
+	String conditions();
+	String[] properties() default {};
+	OrderBy[] sort() default {};
+}
---
+++ kigi-data/src/main/java/tw/kigi/data/annotation/OrderBy.java
@@ -0,0 +1,12 @@
+package tw.kigi.data.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OrderBy {
+	String property();
+	String direction();
+}
